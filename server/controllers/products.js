cbbfa0e710cfd0422c9818975a63cf5a1a9fd589
Lane Sorell <lanesorell@gmail.com>
2019-05-10 15:59:57 -0700

    Added more product route function

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,28 +1,83 @@
 const knex = require('../knex')
-/*
-const formatProduct = (product) => {    
-    var formattedProduct = {
-        productId: product.product_id,
-        productCategoryId: product.product_category_id,
-        imageUrl: product.image_url,
-        price: product.price,
-        wholesalePrice: product.wholesale_price,
-        productDescription: product.product_description
-}
-return formatProduct}
-*/
+
+const intId = (req, property) => parseInt(req.params[property], 10)
+
 const formatProduct = product => ({
-    productId: product.product_id,
-    productCategoryId: product.product_category_id,
+    productId: product.product_id, 
     imageUrl: product.image_url,
     price: product.price,
-    wholesalePrice: product.wholesale_price,
-    productDescription: product.product_description
+    isSale: product.is_sale    
+})
+
+const formatDesc = desc => ({
+    description: desc.product_description
 })
 
 exports.getAllProducts = async (req, res) => {
     const dirtyProducts = await knex.call('GetAllProducts')    
-    const products = await dirtyProducts[0][0].map(formatProduct)
-    console.dir(products)
+    const products = await dirtyProducts[0][0].map(formatProduct)    
     return res.json(products)
 }
+
+exports.getBaby = async (req, res) => {
+    const dirtyProducts = await knex.call('GetBaby')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    return res.json(products)
+}
+
+exports.getBirthday = async (req, res) => {
+    const dirtyProducts = await knex.call('GetBirthday')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    return res.json(products)
+}
+
+exports.getCongrats = async (req, res) => {
+    const dirtyProducts = await knex.call('GetCongratulations')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    return res.json(products)
+}
+
+exports.getFriendEveryday = async (req, res) => {
+    const dirtyProducts = await knex.call('GetFriendshipEveryday')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    return res.json(products)
+}
+
+exports.getHoliday = async (req, res) => {
+    const dirtyProducts = await knex.call('GetHoliday')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    return res.json(products)
+}
+
+exports.getLove = async (req, res) => {
+    const dirtyProducts = await knex.call('GetLove')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    return res.json(products)
+}
+
+exports.getMomDad = async (req, res) => {
+    const dirtyProducts = await knex.call('GetMomsDads')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    return res.json(products)
+}
+
+exports.getSympathy = async (req, res) => {
+    const dirtyProducts = await knex.call('GetSympathy')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    return res.json(products)
+}
+
+exports.getThanks = async (req, res) => {
+    const dirtyProducts = await knex.call('GetThankYou')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    return res.json(products)
+}
+
+exports.getProduct = async (req, res) => {
+    const dirtyDesc = await knex.call(`GetProduct(${intId(req, 'id')})`)
+    if (!dirtyDesc[0][0][0]) {
+      return res.status(404).send('Card not found')
+    }
+    const desc = formatDesc(dirtyDesc[0][0][0])
+    return res.json(desc)
+  }
--- server/routes/products.js
+++ server/routes/products.js
@@ -3,6 +3,16 @@ const {catchErrors} = require('../handlers/errorHandlers')
 
 function setupProductRoutes(router) {
     router.get('/', catchErrors(productController.getAllProducts))
+    router.get('/baby', catchErrors(productController.getBaby))
+    router.get('/birthday', catchErrors(productController.getBirthday))
+    router.get('/congratulations', catchErrors(productController.getCongrats))
+    router.get('/friend-everyday', catchErrors(productController.getFriendEveryday))
+    router.get('/holiday', catchErrors(productController.getHoliday))
+    router.get('/love', catchErrors(productController.getLove))
+    router.get('/moms-dads', catchErrors(productController.getMomDad))
+    router.get('/sympathy', catchErrors(productController.getSympathy))
+    router.get('/thank-you', catchErrors(productController.getThanks))
+    router.get('/:id', catchErrors(productController.getProduct))
 }
 
 module.exports = setupProductRoutes
