7c08a07299fce94892063bb3ec2671df2a1c7d76
Lane Sorell <lanesorell@gmail.com>
2019-05-09 22:04:00 -0700

    Fixed api return issue.

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,5 +1,16 @@
 const knex = require('../knex')
-
+/*
+const formatProduct = (product) => {    
+    var formattedProduct = {
+        productId: product.product_id,
+        productCategoryId: product.product_category_id,
+        imageUrl: product.image_url,
+        price: product.price,
+        wholesalePrice: product.wholesale_price,
+        productDescription: product.product_description
+}
+return formatProduct}
+*/
 const formatProduct = product => ({
     productId: product.product_id,
     productCategoryId: product.product_category_id,
@@ -10,7 +21,8 @@ const formatProduct = product => ({
 })
 
 exports.getAllProducts = async (req, res) => {
-    //const dirtyProducts = await knex.call('GetAllProducts')
-    //const products = dirtyProducts.map(formatProduct)
-    return res.send('hello')
+    const dirtyProducts = await knex.call('GetAllProducts')    
+    const products = await dirtyProducts[0][0].map(formatProduct)
+    console.dir(products)
+    return res.json(products)
 }
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const setupProductRoutes = require('./products')
 function setupRoutes(app) {
     const productRouter = express.Router()
     setupProductRoutes(productRouter)
-    app.use('api/products', productRouter)
+    app.use('/api/products', productRouter)
 }
 
 module.exports = setupRoutes
