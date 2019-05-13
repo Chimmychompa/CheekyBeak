b1714f33787af9714d3be91f51124249ce123492
Lane Sorell <lanesorell@gmail.com>
2019-05-13 13:32:50 -0700

    Changed cors.

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,7 +4,7 @@ const setupProductRoutes = require('./products')
 function setupRoutes(app) {
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
 
