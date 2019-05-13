035dabe6791e35278123679300a8712e5766550a
Lane Sorell <lanesorell@gmail.com>
2019-05-13 13:07:50 -0700

    Changed access control

--- server/app.js
+++ server/app.js
@@ -15,4 +15,10 @@ if(isDevelopment) {
 }
 app.use(errorHandlers.productionErrors)
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 module.exports = app
